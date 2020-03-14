8d9313362e1d793270d191b73261a92e0d7aada7
Jeremy Barnes <jeremyb1988@gmail.com>
2020-03-14 15:50:13 -0700

    small updates to theme node.js

--- packages/theme/gatsby-node.js
+++ packages/theme/gatsby-node.js
@@ -3,10 +3,22 @@ const path = require("path");
 
 //  this is integral to call, b/c it allows your theme to pull in any data from your data folder for your source nodes
 exports.onPreBootstrap = () => {
-	const contentPath = "src/data";
-	if (!fs.existsSync(contentPath)) {
+	const contentPath = ["src", "src/data", "src/data/images", "src/sitePages"];
+	if (!fs.existsSync(contentPath[0])) {
 		console.log(`creating the CONTENT PATH ${contentPath} directory`);
-		fs.mkdirSync(`${contentPath}`);
+		fs.mkdirSync(`${contentPath[0]}`);
+	}
+	if (!fs.existsSync(contentPath[1])) {
+		console.log(`creating the CONTENT PATH ${contentPath} directory`);
+		fs.mkdirSync(`${contentPath[1]}`);
+	}
+	if (!fs.existsSync(contentPath[2])) {
+		console.log(`creating the CONTENT PATH ${contentPath} directory`);
+		fs.mkdirSync(`${contentPath[2]}`);
+	}
+	if (!fs.existsSync(contentPath[3])) {
+		console.log(`creating the CONTENT PATH ${contentPath} directory`);
+		fs.mkdirSync(`${contentPath[3]}`);
 	}
 };
 
